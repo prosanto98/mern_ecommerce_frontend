a9555e50d55a67ace5f3c887eaeaf56a5d3d9fdf
Prosanto Deb <antikontudeb@gmail.com>
2024-08-27 02:44:59 +0600

    Navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} >
+        <Route path=":productId" element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<Order/>} />
+        <Route path="/verify" element={<Verify />} />
+        <Route path="/myorders" element={<MyOrders/>} />
+      </Routes>
     </BrowserRouter>
   )
 }
